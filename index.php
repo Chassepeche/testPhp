5edbbe489660c2e68cc6be0456dc0e908a49dea0
Louis <louis.ducourneau@gmail.com>
2025-12-26 21:44:39 +0100

    premieres pages

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    //HomePage : doit proposer un moyen de se connecter.
+    //doit aussi proposer de voir les posts des gens.
+    echo "Hello world";
+
+
+?>
---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "louisducourneau_user-db-social";
+$password = "haMyuHr8Oa$h";
+$dbname = "louisducourneau_mini-social";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>

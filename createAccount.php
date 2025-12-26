30e37248102ab9b9fadfaa55643c666ed349be86
Louis <louis.ducourneau@gmail.com>
2025-12-26 23:49:48 +0100

    login page updated

--- createAccount.php
+++ createAccount.php
@@ -15,7 +15,7 @@ $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 // Requête préparée
 $sql = "INSERT INTO users (username, email, password) VALUES ($username,$email, $password)";
-
+$conn->query($sql);
 header("Location: login.php");
 
 
--- register.php
+++ register.php
@@ -3,12 +3,11 @@
 
     <form action="createAccount.php" method="post"> username : <input type="text" name="user">
       <br>
-      E-mail: <input type="text" name="email">
+      E-mail: <input type="email" name="email">
       <br>
-      Password : <input type="text" name="password">
+      Password : <input type="password" name="password">
       <input type="submit">
     </form>
 
   </body>
 </html>
-?>

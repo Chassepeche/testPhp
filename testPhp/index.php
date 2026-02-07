36439f4828e20412532b73d42f8790e172845e59
Louis <louis.ducourneau@gmail.com>
2026-02-07 18:59:28 +0100

    base de donnée

--- testPhp/index.php
+++ testPhp/index.php
@@ -3,7 +3,22 @@
 <body>
  
 <?php
-echo 'My first PHP script!';
+//connection BDD
+
+$db_name = "louisducourneau_testPHP";
+$db_username = "louisducourneau_phpUser";
+$db_password = "t%^Rw{NFfC0;";
+$db_servername = "localhost";
+
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+
+if($conn->connect_error){
+    die("Connection failed")
+}
+
+$username = SELECT username FROM users WHERE ID = 1;
+
+echo "Hello $username";
 ?>
 
 </body>

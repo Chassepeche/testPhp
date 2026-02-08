90b54abbe8149ed4f04208c0177edc08f5022a44
Louis <louis.ducourneau@gmail.com>
2026-02-08 13:22:35 +0100

    base de donnée

--- testPhp/index.php
+++ testPhp/index.php
@@ -13,10 +13,10 @@ $db_servername = "localhost";
 $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
 
 if($conn->connect_error){
-    die("Connection failed")
+    die("Connection failed");
 }
 
-$username = SELECT username FROM users WHERE ID = e786aa2b-044e-11f1-a246-b47af137ac0c;
+$username = "SELECT username FROM users WHERE ID = e786aa2b-044e-11f1-a246-b47af137ac0c";
 
 echo "Hello $username";
 ?>

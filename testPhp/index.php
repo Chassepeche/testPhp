8d4d54a0a39e919a21bccc02864b812730f067c0
Louis <louis.ducourneau@gmail.com>
2026-02-10 19:18:46 +0100

    base de donnée

--- testPhp/index.php
+++ testPhp/index.php
@@ -16,14 +16,14 @@ if($conn->connect_error){
     die("Connection failed");
 }
 
-$sql = "SELECT username FROM users WHERE ID = 'e786aa2b-044e-11f1-a246-b47af137ac0c'";
+$sql = "INSERT INTO `users` (`ID`, `username`, `password`, `sudo`) VALUES (NULL, 'test', 'salut', '0')";
 
 $result = $conn->query($sql);
 
 $row = $result->fetch_assoc();
 //echo $row;
 
-echo "Hello $username";
+echo "Hello";
 ?>
 
 </body>

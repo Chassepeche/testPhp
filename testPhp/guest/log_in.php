338aa2679e6a510e284d8934b5cab5a295e1af13
Louis <louis.ducourneau@gmail.com>
2026-02-14 00:49:44 +0100

    connexion bdd

---
+++ testPhp/guest/log_in.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="verifyUser.php" method="POST">
+            <input type="text" id="username"/>
+            <input type="text" id="password"/>
+            <button type="submit">Se connecter</button>
+        </form>
+    </body>
---
+++ testPhp/guest/verifyUser.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php require "../config.php";
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT password FROM users WHERE username = $username";
+
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $hash = $row['password'];
+    $isPassWordCorrect = password_verify($password, $hash);
+
+    echo($isPassWordCorrect);
+
+?>
+<html>
+    
+
+</html>

1342f2b2fbae736f76b9c14204f7d2cff3ae01d4
Louis <louis.ducourneau@gmail.com>
2026-02-13 23:43:15 +0100

    creation d'une page de connexion

---
+++ testPhp/guest/createUser.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>Test page createAccount</title>
+    </head> 
+    <body>
+        <h1>salut</h1>
+    </body>
+</html>
--- testPhp/guest/sign_in.php
+++ testPhp/guest/sign_in.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html>
-    <head><title>test</title></head>
-    <body>salut test login</body>
+    <head><title>S'inscrire</title></head>
+    <body>
+        <form action="createUser.php" method="POST">
+            <input type="text" id="username" />
+            <input type="text" id="password" />
+            <button type="submit">S'inscrire</button>
+        </form>
+    </body>
 </html>

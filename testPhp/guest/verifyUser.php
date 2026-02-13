f244ba486b57fd10e85771cc1896496006fabcd7
Louis <louis.ducourneau@gmail.com>
2026-02-14 00:59:52 +0100

    jsui fatigay

--- testPhp/guest/verifyUser.php
+++ testPhp/guest/verifyUser.php
@@ -5,8 +5,10 @@
     $password = $_POST['password'];
 
     $sql = "SELECT password FROM users WHERE username = '$username'";
+    echo "SQL: $sql<br>";
 
     $result = $conn->query($sql);
+    echo "Résultat trouvé: " . $result->num_rows . "<br>";
     $row = $result->fetch_assoc();
     $hash = $row['password'];
     $isPassWordCorrect = password_verify($password, $hash);
